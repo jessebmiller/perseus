1fb6191f6427b40c92e2f7fd9211ef9bd639d034
Jesse Miller <jesse.miller@adops.com>
2016-12-02 13:45:34 -0600

    bugfix

--- libs/go/perseus/go_perseus.go
+++ libs/go/perseus/go_perseus.go
@@ -20,7 +20,7 @@ func send(message string) {
 	host := cfg.Get("PERSEUS_URL", "http://perseus")
 	ns := cfg.Get("PERSEUS_NS", "/default")
 	vals := url.Values{"message": {message}}
-	_, _ := http.Post(
+	http.Post(
 		host+ns,
 		"application/x-www-form-urlencoded",
 		bytes.NewBufferString(vals.Encode()),
