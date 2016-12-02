93cc19ff5ddfe5ee22f5bcd894c3631d3771bd46
Jesse Miller <jesse.miller@adops.com>
2016-12-02 16:38:07 -0600

    adds untested python lib

---
+++ libs/python/pyrseus/__init__.py
@@ -0,0 +1,4 @@
+# package
+
+import send
+import sendf
---
+++ libs/python/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name = "pyrseus",
+    version = "0.0.1",
+    author = "Jesse B. Miller",
+    author_email = "jesse.miller@adops.com",
+    description = "python perseus client",
+    license = "MIT",
+    url = "http://github.com/oaodev/perceus/libs/python/pyrseus",
+    packages = ["pyrseus"],
+    long_description = "jus a python perseus client",
+)
